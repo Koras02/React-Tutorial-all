5af26060cb2278d1408813d7016a955ac8915d6b
Koras02 <wjdgh0727@gmail.com>
2020-11-11 20:17:36 +0900

    4:46:23

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import UseStateObject from './tutorial/1-useState/setup/4-useState-object';
 import Setup from './tutorial/1-useState/setup/4-useState-object';
+import Final from './tutorial/1-useState/final/3-useState-array';
 
 function App() {
   return (
   <div className='container'>
-      <Setup />
+      <Final />
    </div>
   );
 }
